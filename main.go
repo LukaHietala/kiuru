75707ba913ba7e916d3004613c49b1d0f814e12f
Luka Hietala <luka.hietala08@gmail.com>
2026-08-30 13:27:47 +0300

    Add tcell and render basic things

--- main.go
+++ main.go
@@ -1,7 +1,49 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+
+	"github.com/gdamore/tcell/v3"
+)
 
 func main() {
-	fmt.Println("Hello")
+	s, err := tcell.NewScreen()
+	if err != nil {
+		log.Fatal(err)
+	}
+	if err := s.Init(); err != nil {
+		log.Fatal(err)
+	}
+	s.EnableMouse()
+	s.Clear()
+
+	defer func() {
+		r := recover()
+		s.Fini()
+		if r != nil {
+			panic(r)
+		}
+	}()
+
+	for {
+		s.Show()
+
+		ev := <-s.EventQ()
+
+		switch ev := ev.(type) {
+		case *tcell.EventResize:
+			s.Sync()
+		case *tcell.EventKey:
+			if ev.Key() == tcell.KeyEscape || ev.Key() == tcell.KeyCtrlC {
+				return
+			}
+			if ev.Key() == tcell.KeyRune {
+				s.Put(0, 0, ev.Str(), tcell.StyleDefault)
+			}
+		case *tcell.EventMouse:
+			x, y := ev.Position()
+			s.PutStr(0, 0, fmt.Sprintf("Rotta: %d %d", x, y))
+		}
+	}
 }
